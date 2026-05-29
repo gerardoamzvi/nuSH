6ed9d8b3aa507b2e81adb9e13ae620533d472a89
gerardoamzvi <111869677+gerardoamzvi@users.noreply.github.com>
2026-05-29 15:35:30 +0200

    Started implementing user input handling

--- main.c
+++ main.c
@@ -2,7 +2,43 @@
 #include <stdlib.h>
 #include <string.h>
 
+void execute(char * input, const char * separators){
+
+    while (input != NULL)
+    {
+        printf("Executing %s\n", input);
+        input = strtok(NULL, separators);
+    }
+    
+}
+
 int main() {
-    printf("Hello, World!\n");
+    printf("You are in the nuSH Shell!\n");
+    char buff[100];
+
+    while (1){
+
+    printf("C:> ");
+    fgets(buff, sizeof(buff), stdin);
+
+    // find the \n from fgets and replace value with '\0'
+    buff[strcspn(buff, "\n")] = '\0';
+
+    if(strncmp(buff, "exit", 4) == 0){
+        printf("Goodbye");
+        break;
+    }  
+       else if (strncmp(buff, "help", 4) == 0){
+        printf("THis is going to help you.\n");
+        continue;
+    } 
+    else{
+        const char * separators = " -/";
+        char * pBuff = strtok(buff, separators);
+        execute(pBuff, separators);
+    }
+    }
+
+    
     return 0;
 }
